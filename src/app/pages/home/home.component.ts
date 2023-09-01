1a480f49b78921583d8bb98a554394e84c0e7954
Javier Rodríguez Valentín <javrova@gmail.com>
2023-09-01 23:26:45 +0200

    User card development

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Resource } from 'src/app/interfaces/resource.interface';
+import { User } from 'src/app/interfaces/user.interface';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +9,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  objResources: Resource;
+  arrUsers: User[] = [];
+  usersServices = inject(UsersService);
+
+  constructor() {
+    this.objResources = {
+      "page": 0,
+      "per_page": 0,
+      "total": 0,
+      "total_pages": 0,
+      "results": [],
+    }
+  }
+
+  async ngOnInit(): Promise<void> {
+    var a: User | any;
+    try {
+      this.objResources = await this.usersServices.getAll();
+      this.arrUsers = this.objResources.results
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
 
 }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { User } from '../interfaces/user.interface';
+import { Resource } from '../interfaces/resource.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+  
+  httpClient = inject(HttpClient);
+  private baseUrl = "https://peticiones.online/api/users/"
 
   constructor() { }
+
+  getAll():Promise<Resource>{
+    return lastValueFrom(this.httpClient.get<Resource>(this.baseUrl));
+  }
+
 }
